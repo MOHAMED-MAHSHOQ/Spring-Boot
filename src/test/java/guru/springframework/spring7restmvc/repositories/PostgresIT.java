0aa148abe6e5453fbee33bbc3d7ce8d3ecafa6e1
MOHAMED-MAHSHOQ <msmahshoq2005@gmail.com>
2026-03-22 09:05:10 +0530

    Day 12 Completed Test Container Section Udemy

---
+++ src/test/java/guru/springframework/spring7restmvc/repositories/PostgresIT.java
@@ -0,0 +1,35 @@
+package guru.springframework.spring7restmvc.repositories;
+
+import guru.springframework.spring7restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("localpostgres")
+public class PostgresIT {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgresContainer =
+            new PostgreSQLContainer<>("postgres:16");
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<Beer> beers = beerRepository.findAll();
+        assertThat(beers.size()).isGreaterThan(0);
+    }
+}
